0fd570d4e5c84bb189314ab1b26c366223e38f70
Shivam Bhardwaj <Shivambhardwaj8585@gmail.com>
2024-02-26 20:34:19 +0530

    users dummy data added

--- index.js
+++ index.js
@@ -1,24 +1,100 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const userData = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+];
 
-app.get('/twitter', (req, res) => {
-    res.send('thisisshivamdot.com')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/login', (req, res) => {
-    res.send('<h1>Please login at twitter!</h1>')
-})
+app.get("/twitter", (req, res) => {
+  res.send("thisisshivamdot.com");
+});
 
-app.get('/youtube', (req, res) => {
-    res.send('<h2>Chai aur code</h2>')
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login at twitter!</h1>");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Chai aur code</h2>");
+});
+
+app.get('/users', (req, res) => {
+    res.json(userData)
 })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
